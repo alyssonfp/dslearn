263fda8062561bd1d97f058f702b16fa561c36a8
alyssonfp <alyssonfp@gmail.com>
2021-07-10 17:58:54 -0300

    Lessons seed, fi: lessonsDone

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -41,7 +41,7 @@ public abstract class Lesson implements Serializable {
 				@JoinColumn(name = "offer_id")
 		}
 	)
-	private Set<Enrollment> enrollmentDone = new HashSet<>();
+	private Set<Enrollment> enrollmentsDone = new HashSet<>();
 	
 	public Lesson() {
 		
@@ -88,7 +88,7 @@ public abstract class Lesson implements Serializable {
 	}
 
 	public Set<Enrollment> getEnrollmentDone() {
-		return enrollmentDone;
+		return enrollmentsDone;
 	}
 
 	@Override
